71843e607bd550243a94308b884399930f4bd58c
Raphael Tang <raphpb1912@gmail.com>
2020-10-17 23:33:27 +0800

    increase top p

--- brain/brain/__main__.py
+++ brain/brain/__main__.py
@@ -101,7 +101,7 @@ async def root(body: RootBody):
         use_cache=True,
         top_k=40,
         temperature=0.7,
-        top_p=0.5,
+        top_p=0.6,
     )
 
     bot_message: str = tokenizer.decode(bot_outputs[0], skip_special_tokens=False)
