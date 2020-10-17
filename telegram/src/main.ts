0eb80624da99ffb745d5c9fc2e7407365b031941
Raphael Tang <raphpb1912@gmail.com>
2020-10-18 00:28:52 +0800

    changed to websockets

--- telegram/src/main.ts
+++ telegram/src/main.ts
@@ -1,68 +1,80 @@
 require("dotenv").config();
 import { Telegraf, Extra } from "telegraf";
-import got from "got";
+import WebSocket from "ws";
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
-const EOS_TOKEN = "<|endoftext|>";
-let maxTurnsHistory = 2;
-let turns: Turn[] = [];
+// const EOS_TOKEN = "<|endoftext|>";
+// let maxTurnsHistory = 2;
+// let turns: Turn[] = [];
 
 bot.start((ctx) => {
-  turns = [];
+  // turns = [];
   ctx.reply("Hey, what's up?");
 });
 
-async function chat(message: string) {
-  if (maxTurnsHistory === 0) {
-    turns = [];
-    return;
-  }
+const ws = new WebSocket(process.env.BRAIN_URL);
 
-  const turn: Turn = {
-    userMessages: [],
-    botMessages: [],
-  };
-  turns.push(turn);
-  turn.userMessages.push(message);
-  let prompt = "";
-  const fromIndex =
-    maxTurnsHistory >= 0 ? Math.max(turns.length - maxTurnsHistory - 1, 0) : 0;
-  for (const turn of turns.slice(fromIndex)) {
-    for (const userMessage of turn.userMessages) {
-      prompt += userMessage + EOS_TOKEN;
-    }
-    for (const botMessage of turn.botMessages) {
-      prompt += botMessage + EOS_TOKEN;
-    }
-  }
+ws.on("open", () => {
+  console.log("Websocket opened");
+});
 
-  console.log(prompt);
+function sendMessage(message: string) {
+  // if (maxTurnsHistory === 0) {
+  //   turns = [];
+  //   return;
+  // }
+  //
+  // const turn: Turn = {
+  //   userMessages: [],
+  //   botMessages: [],
+  // };
+  // turns.push(turn);
+  // turn.userMessages.push(message);
+  // let prompt = "";
+  // const fromIndex =
+  //   maxTurnsHistory >= 0 ? Math.max(turns.length - maxTurnsHistory - 1, 0) : 0;
+  // for (const turn of turns.slice(fromIndex)) {
+  //   for (const userMessage of turn.userMessages) {
+  //     prompt += userMessage + EOS_TOKEN;
+  //   }
+  //   for (const botMessage of turn.botMessages) {
+  //     prompt += botMessage + EOS_TOKEN;
+  //   }
+  // }
+  //
+  // console.log(prompt);
+  //
+  // const botMessage = await (async () => {
+  //   const response = await got.post<{ bot_message: string }>(
+  //     process.env.BRAIN_URL,
+  //     {
+  //       json: { prompt: prompt },
+  //       responseType: "json",
+  //     }
+  //   );
+  //   return response.body.bot_message;
+  // })();
+  //
+  // turn.botMessages.push(botMessage);
 
-  const botMessage = await (async () => {
-    const response = await got.post<{ bot_message: string }>(
-      process.env.BRAIN_URL,
-      {
-        json: { prompt: prompt },
-        responseType: "json",
-      }
-    );
-    return response.body.bot_message;
-  })();
-
-  console.log(botMessage);
-
-  turn.botMessages.push(botMessage);
-
-  return botMessage;
+  ws.send(message, (err) => {
+    if (err) throw err;
+  });
 }
 
 bot.on("text", async (ctx) => {
   try {
     if (ctx.chat.type === "private") {
       ctx.telegram.sendChatAction(ctx.chat.id, "typing");
+
       console.log(`Recieved: ${ctx.message.text} (private)`);
-      const reply = await chat(ctx.message.text);
-      await ctx.reply(reply);
+      sendMessage(ctx.message.text);
+
+      ws.on("message", (data) => {
+        if (data instanceof String) {
+          ctx.reply(data as string);
+        }
+      });
     }
   } catch (e) {
     console.log(e);
