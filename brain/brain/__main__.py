79bda227a864b4fd908cedc264e60a6c2e5655c8
Raphael Tang <raphpb1912@gmail.com>
2020-10-19 01:30:16 +0800

    move to pytorch

--- brain/brain/__main__.py
+++ brain/brain/__main__.py
@@ -1,58 +1,16 @@
 from typing import Any, Dict, List
 from transformers import (
     AutoTokenizer,
-    TFAutoModelForCausalLM,
+    AutoModelForCausalLM,
 )
-from fastapi import FastAPI, WebSocket
+from fastapi import FastAPI
 from pydantic import BaseModel
 import uvicorn
 import os
 
 tokenizer: Any = AutoTokenizer.from_pretrained("microsoft/DialoGPT-medium")
-model: Any = TFAutoModelForCausalLM.from_pretrained("microsoft/DialoGPT-medium")
-# file_path = "./data.txt"
+model: Any = AutoModelForCausalLM.from_pretrained("microsoft/DialoGPT-medium")
 app = FastAPI()
-
-
-# training_args = TFTrainingArguments(
-#     output_dir="./results",  # output directory
-#     num_train_epochs=3,  # total # of training epochs
-#     per_device_train_batch_size=16,  # batch size per device during training
-#     per_device_eval_batch_size=64,  # batch size for evaluation
-#     warmup_steps=500,  # number of warmup steps for learning rate scheduler
-#     weight_decay=0.01,  # strength of weight decay
-#     logging_dir="./logs",  # directory for storing logs
-# )
-
-# # build datasets
-# dataset = TextDataset(
-#     tokenizer=tokenizer,
-#     file_path=file_path,
-#     block_size=-1,
-#     overwrite_cache=False,
-#     cache_dir=None,
-# )
-# data_collator = DataCollatorForLanguageModeling(
-#     tokenizer=tokenizer, mlm=False, mlm_probability=0.15
-# )
-
-# model.resize_token_embeddings(len(tokenizer))
-
-# # init trainer
-# trainer = TFTrainer(
-#     model=model,
-#     data_collator=data_collator,
-#     args=training_args,
-#     train_dataset=dataset,
-#     prediction_loss_only=True,
-# )
-
-# trainer.train()
-# trainer.save_model()
-
-# print("Output:\n" + 100 * "-")
-# for i, output in enumerate(outputs):
-#     print("{}: {}".format(i, tokenizer.decode(output, skip_special_tokens=True)))
 
 
 max_turns_history = 2
@@ -93,7 +51,7 @@ async def root(body: RootBody):
     print(f"Prompt: {prompt}")
 
     # generate bot messages
-    input_ids = tokenizer.encode(prompt, return_tensors="tf")
+    input_ids = tokenizer.encode(prompt, return_tensors="pt")
 
     bot_outputs = model.generate(
         input_ids,
